96ff57fd7c0ca885327b9cef7b4e0e8809aff559
Levente Krizsán <lewalevi@gmail.com>
2024-09-28 22:25:51 +0200

    Add zsh config

---
+++ dot_config/zsh/alias.zsh
@@ -0,0 +1,18 @@
+alias t='tmux'
+alias vim='nvim'
+alias v='nvim'
+
+alias ls='ls -hN --color=auto --group-directories-first'
+alias grep='grep --color=auto'
+alias diff='diff --color=auto'
+
+alias l='ls'
+alias ll='ls -lh'
+alias la='ls -lah'
+alias sdn='sudo shutdown -h now'
+alias srn='sudo reboot -h now'
+
+alias ..='cd ..'
+alias ...='cd .. && cd ..'
+
+alias lg='lazygit'
