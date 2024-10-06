b7cdf36db511db3df08ea7a8acb74639434e1553
Levente Krizsán <lewalevi@gmail.com>
2024-10-06 15:34:35 +0200

    Fix git info in tmux status

--- dot_config/tmux/executable_git_info.sh
+++ dot_config/tmux/executable_git_info.sh
@@ -3,7 +3,16 @@
 BRANCH=$(git -C $1 rev-parse --abbrev-ref HEAD)
 
 if [ $? -eq 0 ]; then
-	echo " $BRANCH "
+	max_length=$(tmux show-option -gv status-right-length)
+	# ellipsis="..."
+	ellipsis="…"
+	ellipsis_length=${#ellipsis}
+	if [ ${#BRANCH} -gt $((max_length - ellipsis_length - 2)) ]; then
+		truncated_branch="${BRANCH:0:$((max_length - ellipsis_length - 2))}$ellipsis"
+	else
+		truncated_branch="$BRANCH"
+	fi
+	echo " $truncated_branch "
 else
 	echo ""
 fi
