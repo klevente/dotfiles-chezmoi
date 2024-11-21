fdb43a978f67d0e6841010c7cd9d0beb59b00d15
Levente Krizsán <lewalevi@gmail.com>
2024-11-21 11:41:58 +0100

    Update aliases

--- dot_config/zsh/custom/luzmo.zsh
+++ dot_config/zsh/custom/luzmo.zsh
@@ -1,4 +1,4 @@
 alias dcl='docker compose --profile frontend'
 alias dclb='docker compose --profile frontend build'
 alias luzmo-docker-no-qe2='docker compose --profile frontend up -d --scale qe2=0'
-alias luzmo-docker-no-qe2-be='docker compose --profile frontend --env-file .env.be.local up -d --scale qe2=0'
+alias luzmo-docker-no-qe2-be='docker compose --profile frontend --env-file .env --env-file .env.be.local up -d --scale qe2=0'
